88e9915088da2c9add5814a48495121353ae313c
Michael Vaughn <michael.vaughn@yale.edu>
2023-10-19 15:45:08 -0400

    Fixes mispelling, formats with prettier, removes unused variables

--- tests/keyboard-widget.spec.ts
+++ tests/keyboard-widget.spec.ts
@@ -1,9 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 // Safari uses Alt+Tab vs Tab for webkit
 function tabKeyForBrowser(browserType: string) {
   let key = "Tab";
-  if (browserType === 'webkit') {
+  if (browserType === "webkit") {
     key = "Alt+Tab";
   }
 
@@ -12,36 +12,40 @@ function tabKeyForBrowser(browserType: string) {
 
 // Set globally so we can reuse this throughout our tests.
 let tabKey: string;
-test.beforeEach(async ({ page, defaultBrowserType }) => {
+test.beforeEach(async ({ defaultBrowserType }) => {
   // Webkit uses Alt+Tab over Tab to traverse, so we capture this before a test is done.
   tabKey = tabKeyForBrowser(defaultBrowserType);
 });
 
-test.describe('Keyboard widget', () => {
-  test.beforeEach(async ({ page, defaultBrowserType }) => {
-    await page.goto('https://playwright.dev/');
-    await page.waitForLoadState('domcontentloaded');
+test.describe("Keyboard widget", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://playwright.dev/");
+    await page.waitForLoadState("domcontentloaded");
     // Note: Must target body first in order for proper tabbing to happen.
-    page.locator('body');
+    page.locator("body");
   });
 
   test('Keyboard can get to "Skip to main content" link', async ({ page }) => {
     await page.keyboard.press(tabKey);
     // Copy/pasted selector from dev tools.
-    await expect(page.locator('#__docusaurus > div:nth-child(2) > a')).toBeFocused();
-    await expect(page.locator('#__docusaurus > div:nth-child(2) > a')).toHaveText('Skip to main content');
-  }); 
+    await expect(
+      page.locator("#__docusaurus > div:nth-child(2) > a")
+    ).toBeFocused();
+    await expect(
+      page.locator("#__docusaurus > div:nth-child(2) > a")
+    ).toHaveText("Skip to main content");
+  });
 
-  test('Keyboard can expand menu link groups', async ({ page, isMobile }) => {
+  test("Keyboard can expand menu link groups", async ({ page, isMobile }) => {
     if (isMobile) {
-      // Note: as of the writing of this test, this WILL fail on mobile. 
+      // Note: as of the writing of this test, this WILL fail on mobile.
       // Playwright's site does not target the menu to be expanded on mobile
       // using keyboard.
-      // This is to illistrate how to handle dfferent cases for mobile and
+      // This is to illustrate how to handle dfferent cases for mobile and
       // desktop.
       await page.keyboard.press(tabKey);
       await page.keyboard.press(tabKey);
-      await page.keyboard.press('Enter');
+      await page.keyboard.press("Enter");
       await page.keyboard.press(tabKey);
       await page.keyboard.press(tabKey);
       await page.keyboard.press(tabKey);
@@ -53,14 +57,19 @@ test.describe('Keyboard widget', () => {
       await page.keyboard.press(tabKey);
       await page.keyboard.press(tabKey);
     } else {
+      // This test will pass. Desktop navigation works as expected with keyboard.
       await page.keyboard.press(tabKey);
       await page.keyboard.press(tabKey);
       await page.keyboard.press(tabKey);
       await page.keyboard.press(tabKey);
       await page.keyboard.press(tabKey);
-      await page.keyboard.press('Enter');
+      await page.keyboard.press("Enter");
     }
     // Copy/pasted label from Playwright UI's target feature.
-    await expect(page.getByLabel('Main', { exact: true }).getByRole('link', { name: 'Java' })).toBeVisible();
-  }); 
+    await expect(
+      page
+        .getByLabel("Main", { exact: true })
+        .getByRole("link", { name: "Java" })
+    ).toBeVisible();
+  });
 });
