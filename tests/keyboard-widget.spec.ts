5dc022d6ba6ff0b547d031f142835645ffa9c45b
David Blankenship <david.blankenship@yale.edu>
2023-10-19 09:23:33 -0400

    fix(keyboard-widget): correctly identify tab
    Safari/webkit uses ALT+TAB instead of TAB to propertly tab through
    items.  As such, a method is created to determine this before each test
    is executed using the current browser type being tested.
    
    The menu link group test shows how you can use the isMobile to create
    two different pathways of traversal.  Note that currently the mobile
    test fails due to a real issue with playwright.dev where the menu group
    item for language cannot be selected via keyboard input.
    
    Comments were also added to display different ways to get selectors as
    it can be cumbersome sometimes to target the right item.  Examples
    outlined use the DOM selector you'd get from browser tools like
    Chromium, as well as the Playwright UI selector, which will give you an
    appropriate method to use with the selector arguments entered.

--- tests/keyboard-widget.spec.ts
+++ tests/keyboard-widget.spec.ts
@@ -1,34 +1,66 @@
 import { test, expect } from '@playwright/test';
 
+// Safari uses Alt+Tab vs Tab for webkit
+function tabKeyForBrowser(browserType: string) {
+  let key = "Tab";
+  if (browserType === 'webkit') {
+    key = "Alt+Tab";
+  }
+
+  return key;
+}
+
+// Set globally so we can reuse this throughout our tests.
+let tabKey: string;
+test.beforeEach(async ({ page, defaultBrowserType }) => {
+  // Webkit uses Alt+Tab over Tab to traverse, so we capture this before a test is done.
+  tabKey = tabKeyForBrowser(defaultBrowserType);
+});
+
 test.describe('Keyboard widget', () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page, defaultBrowserType }) => {
     await page.goto('https://playwright.dev/');
     await page.waitForLoadState('domcontentloaded');
     // Note: Must target body first in order for proper tabbing to happen.
     page.locator('body');
   });
 
-  test('Keyboard can get to "Skip to main content" link', async ({ page, isMobile }) => {
-    if (isMobile) {
-      return;
-    }
-
-    await page.keyboard.press('Tab');
+  test('Keyboard can get to "Skip to main content" link', async ({ page }) => {
+    await page.keyboard.press(tabKey);
+    // Copy/pasted selector from dev tools.
     await expect(page.locator('#__docusaurus > div:nth-child(2) > a')).toBeFocused();
     await expect(page.locator('#__docusaurus > div:nth-child(2) > a')).toHaveText('Skip to main content');
   }); 
 
   test('Keyboard can expand menu link groups', async ({ page, isMobile }) => {
     if (isMobile) {
-      return;
+      // Note: as of the writing of this test, this WILL fail on mobile. 
+      // Playwright's site does not target the menu to be expanded on mobile
+      // using keyboard.
+      // This is to illistrate how to handle dfferent cases for mobile and
+      // desktop.
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press('Enter');
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+    } else {
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press('Enter');
     }
-
-    await page.keyboard.press('Tab');
-    await page.keyboard.press('Tab');
-    await page.keyboard.press('Tab');
-    await page.keyboard.press('Tab');
-    await page.keyboard.press('Tab');
-    await page.keyboard.press('Enter');
+    // Copy/pasted label from Playwright UI's target feature.
     await expect(page.getByLabel('Main', { exact: true }).getByRole('link', { name: 'Java' })).toBeVisible();
   }); 
 });
