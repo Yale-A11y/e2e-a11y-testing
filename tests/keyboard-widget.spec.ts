14f3645e886e58eba9f16fb7bdb92aea1d219da9
David Blankenship <128765777+dblanken-yale@users.noreply.github.com>
2023-10-20 09:40:11 -0400

    Add keyboard test (#11)
    * add(test): Add keyboard test
    
    Add example keyboard test to show users how they can incorporate
    keyboard-driven testing into their own test cases.
    
    - Use playwright.dev site as the test location:
        - Add a test for "Skip to main content"
        - Add a test for expanding menu groups by enter key
    
    * fix(keyboard-test): use await on expects
    
    * fix(keyboard-widget): correctly identify tab
    
    Safari/webkit uses ALT+TAB instead of TAB to propertly tab through
    items.  As such, a method is created to determine this before each test
    is executed using the current browser type being tested.
    
    The menu link group test shows how you can use the isMobile to create
    two different pathways of traversal.  Note that currently the mobile
    test fails due to a real issue with playwright.dev where the menu group
    item for language cannot be selected via keyboard input.
    
    Comments were also added to display different ways to get selectors as
    it can be cumbersome sometimes to target the right item.  Examples
    outlined use the DOM selector you'd get from browser tools like
    Chromium, as well as the Playwright UI selector, which will give you an
    appropriate method to use with the selector arguments entered.
    
    * Fixes mispelling, formats with prettier, removes unused variables
    
    ---------
    
    Co-authored-by: Michael Vaughn <michael.vaughn@yale.edu>

---
+++ tests/keyboard-widget.spec.ts
@@ -0,0 +1,75 @@
+import { test, expect } from "@playwright/test";
+
+// Safari uses Alt+Tab vs Tab for webkit
+function tabKeyForBrowser(browserType: string) {
+  let key = "Tab";
+  if (browserType === "webkit") {
+    key = "Alt+Tab";
+  }
+
+  return key;
+}
+
+// Set globally so we can reuse this throughout our tests.
+let tabKey: string;
+test.beforeEach(async ({ defaultBrowserType }) => {
+  // Webkit uses Alt+Tab over Tab to traverse, so we capture this before a test is done.
+  tabKey = tabKeyForBrowser(defaultBrowserType);
+});
+
+test.describe("Keyboard widget", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://playwright.dev/");
+    await page.waitForLoadState("domcontentloaded");
+    // Note: Must target body first in order for proper tabbing to happen.
+    page.locator("body");
+  });
+
+  test('Keyboard can get to "Skip to main content" link', async ({ page }) => {
+    await page.keyboard.press(tabKey);
+    // Copy/pasted selector from dev tools.
+    await expect(
+      page.locator("#__docusaurus > div:nth-child(2) > a")
+    ).toBeFocused();
+    await expect(
+      page.locator("#__docusaurus > div:nth-child(2) > a")
+    ).toHaveText("Skip to main content");
+  });
+
+  test("Keyboard can expand menu link groups", async ({ page, isMobile }) => {
+    if (isMobile) {
+      // Note: as of the writing of this test, this WILL fail on mobile.
+      // Playwright's site does not target the menu to be expanded on mobile
+      // using keyboard.
+      // This is to illustrate how to handle dfferent cases for mobile and
+      // desktop.
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press("Enter");
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+    } else {
+      // This test will pass. Desktop navigation works as expected with keyboard.
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press(tabKey);
+      await page.keyboard.press("Enter");
+    }
+    // Copy/pasted label from Playwright UI's target feature.
+    await expect(
+      page
+        .getByLabel("Main", { exact: true })
+        .getByRole("link", { name: "Java" })
+    ).toBeVisible();
+  });
+});
